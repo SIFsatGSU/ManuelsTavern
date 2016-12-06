4308af187f9fd26aa27a0737f9c7f299230a709b
Jack <nanotek2711@yahoo.com>
2016-12-06 03:42:31 -0500

    First commit

---
+++ Assets/Look.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class Look : MonoBehaviour {
+    public Camera playerCamera;
+    public GameObject reticle;
+
+    public float mouseEnable; // 1 = enabled, 0 = disabled.
+    public float xRotationSpeed;
+    public float yRotationSpeed;
+    public float yRotationAngle;
+    public float reticleSize;
+    private float currentYRotation = 0;
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xRotation = xRotationSpeed * Input.GetAxisRaw("Look X") * Time.deltaTime;
+        float yRotation = yRotationSpeed * Input.GetAxisRaw("Look Y") * Time.deltaTime;
+        xRotation += mouseEnable * xRotationSpeed * Input.GetAxisRaw("Mouse X") * Time.deltaTime;
+        yRotation += mouseEnable * yRotationSpeed * Input.GetAxisRaw("Mouse Y") * Time.deltaTime;
+        currentYRotation = Mathf.Clamp(currentYRotation + yRotation, -yRotationAngle / 2, yRotationAngle / 2);
+
+        transform.Rotate(new Vector3(0, 1, 0), xRotation);
+        playerCamera.transform.localEulerAngles = new Vector3(-currentYRotation, 0, 0);
+
+        // Ray cast
+        GameObject[] pictureFrames = GameObject.FindGameObjectsWithTag("PictureFrame");
+        foreach (GameObject frame in pictureFrames) {
+            frame.transform.GetChild(0).GetComponent<Renderer>().enabled = false;
+        }
+
+        Ray forwardRay;
+        if (VRDevice.isPresent) {
+            Vector3 forward = InputTracking.GetLocalRotation(VRNode.Head) * new Vector3(0, 0, 1);
+            forward = transform.rotation * forward;
+            forwardRay = new Ray(playerCamera.transform.position, forward);
+        } else {
+            forwardRay = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        }
+        Debug.DrawRay(forwardRay.origin, forwardRay.direction);
+        RaycastHit hit;
+        if (Physics.Raycast(forwardRay, out hit)) {
+            Vector3 hitProjection = hit.point - playerCamera.transform.position;
+            float distance = hitProjection.magnitude;
+            reticle.transform.localPosition = new Vector3(0, 0, distance);
+            float reticleScale = distance * reticleSize;
+            reticle.transform.localScale = new Vector3(reticleScale, reticleScale, reticleScale);
+            if (hit.transform.gameObject.tag == "PictureFrame") {
+                GameObject pictureFrame = hit.transform.gameObject;
+                GameObject pictureText = pictureFrame.transform.GetChild(0).gameObject;
+                pictureText.GetComponent<Renderer>().enabled = true;
+            }
+        }
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Movement : MonoBehaviour {
+    public CharacterController characterController;
+    public GameObject cameraContainer;
+    public float maxSpeed;
+    public float jumpStrength;
+    public float acceleration;
+    public float normalHeight;
+    public float crouchHeight;
+    private float gravity = .3f;
+    private float ySpeed = 0;
+    public float currentSpeed = 0;
+    public Vector3 currentDirection = new Vector3();
+    private bool crouchMode = false;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (characterController.isGrounded) {
+            ySpeed = 0;
+        }
+        if (characterController.velocity.y == 0) { // Hit ceiling.
+            ySpeed = -.1f;
+        }
+        if (Input.GetButton("Jump") && characterController.isGrounded) {
+            ySpeed = jumpStrength;
+        }
+        if (Input.GetButton("Crouch")) {
+            characterController.height = crouchHeight;
+        } else {
+            characterController.height = normalHeight;
+        }
+        characterController.center = new Vector3(0, characterController.height / 2, 0);
+        cameraContainer.transform.localPosition = new Vector3(0, characterController.height - .15f, 0);
+        Vector3 zMovement = transform.forward * Input.GetAxisRaw("Vertical");
+        Vector3 xMovement = transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3 yMovement = transform.up * ySpeed;
+        if (characterController.velocity.magnitude == 0) {
+            currentSpeed = 0;
+        }
+        if (zMovement.magnitude > 0 || xMovement.magnitude > 0) {
+            currentDirection = zMovement + xMovement;
+            currentDirection.Normalize();
+            currentSpeed = Mathf.Clamp(currentSpeed + acceleration, 0, maxSpeed);
+        } else {
+            currentSpeed = Mathf.Clamp(currentSpeed - acceleration, 0, maxSpeed);
+        }
+        characterController.Move((yMovement + currentDirection * currentSpeed) * Time.deltaTime);
+        ySpeed -= gravity;
+	}
+}
